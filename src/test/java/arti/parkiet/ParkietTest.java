2dfe223088b02a8d3760fc65140316661cef2241
Your Name <you@example.com>
2026-04-14 09:56:23 +0200

    Initial commit

---
+++ src/test/java/arti/parkiet/ParkietTest.java
@@ -0,0 +1,22 @@
+package arti.parkiet;
+
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest(transactional = false)
+class ParkietTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
